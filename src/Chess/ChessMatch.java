7360ec5d1bb2c16e215a2e0b170d73b4d9e9cf86
Bernardo Rodrigues <bernardofigueiredo387@gmail.com>
2024-06-17 17:38:40 -0300

    Knight

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -9,6 +9,7 @@ import BoardGame.Piece;
 import BoardGame.Position;
 import Chess.pieces.Bishop;
 import Chess.pieces.King;
+import Chess.pieces.Knight;
 import Chess.pieces.Pawn;
 import Chess.pieces.Rook;
 
@@ -195,6 +196,9 @@ public class ChessMatch {
 	
 	
 	private void initialSetup() {
+		
+		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
@@ -209,7 +213,8 @@ public class ChessMatch {
 		placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 		
-		
+		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
