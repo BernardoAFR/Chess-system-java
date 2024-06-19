c16961ad010dc5c4046cacc214644f50e56f49a8
Bernardo Rodrigues <bernardofigueiredo387@gmail.com>
2024-06-19 18:54:14 -0300

    Special move - Promotion

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 			
 				if(capturedPiece != null)
 					captured.add(capturedPiece);
+				
+				if(chessmatch.getPromotedPiece() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					String type = input.nextLine();
+					chessmatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
